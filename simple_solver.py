9a24d1d7a4baf6d2e64396b5728e4fee6cc1c996
Amelia Dayton <daytonamelia@gmail.com>
2025-07-21 13:48:36 -0700

    Added solver function

--- simple_solver.py
+++ simple_solver.py
@@ -1,20 +1,21 @@
-"""
+'''
 Simple soduku solver.
-"""
+https://hodoku.sourceforge.net/en/techniques.php
+'''
+# Imports
 from typing import Callable
 
-VALID_NUMS = {'1','2','3','4','5','6','7','8','9'}
-VALID_INPUT = {'1','2','3','4','5','6','7','8','9', '.'}
 
 class Cell:
-    """
-    A single sudoku cell with coordinates x,y.
-    """
+    '''
+    A single sudoku cell with coordinates x, y. '.' indicates an empty cell.
+    '''
     def __init__(self, _x: int, _y: int, _value: str = '.', ) -> None:
         '''Creates a cell with coordinates x,  y and an optional value.'''
         self.x = _x
         self.y =_y
         self.value = _value
+        self.pencil = None if self.value != '.' else []
 
     def __str__(self) -> str:
         '''Returns cell value as a string.'''
@@ -22,16 +23,22 @@ class Cell:
 
     def set(self, _value:str) -> None:
         '''Sets the value of a cell to a new value.'''
-        print(f"Setting cell at ({self.x}, {self.y}) to {_value}")
+        print(f"INFO: Setting cell at ({self.x}, {self.y}) to {_value}")
         self.value = _value
+        
+    def set_pencil(self, marks:list) -> None:
+        '''Set the cell's pencil marks.'''
+        self.pencil = marks
+
 
 class Board:
-    """
-    A sudoku board of 9x9 Cell objects.
-    """
-    def __init__(self) -> None:
-        '''Creates an empty board of nine lists of Cells within a list.'''
-        self.data = [[Cell(x, y) for x in range(9)] for y in range(9)]
+    '''
+    A square sudoku board of nxn Cell objects.
+    '''
+    def __init__(self, _n:int=9) -> None:
+        '''Creates an empty square board of n lists of Cells within a list.'''
+        self.n = _n
+        self.data = [[Cell(x, y) for x in range(self.n)] for y in range(self.n)]
 
     def __str__(self) -> str:
         '''Returns board as space-delineated string with each row as a new line.'''
@@ -44,19 +51,20 @@ class Board:
 
     def set_row(self, values:list, _y:int) -> None:
         '''Given a y-coordinate, fill the row with a list of values.'''
-        for i in range(9):
+        for i in range(self.n):
             self.set_cell(values[i], i, _y)
 
     def check_board(self) -> bool:
         '''Checks if board is valid.'''
-        for i in range(9):
+        valid_nums = {str(i) for i in range(1, self.n + 1)}
+        for i in range(self.n):
             row = self.get_row(i)
             col = self.get_col(i)
             block = self.get_block(i)
-            for num in VALID_NUMS:
-                row_count = row.count(num)
-                col_count = col.count(num)
-                block_count = block.count(num)
+            for num in valid_nums:
+                row_count = sum(1 for cell in row if cell.value == num)
+                col_count = sum(1 for cell in col if cell.value == num)
+                block_count = sum(1 for cell in block if cell.value == num)
                 if row_count > 1 or col_count > 1 or block_count > 1:
                     print(f"On {num}: {row_count} rows, {col_count} cols, {block_count} blocks.")
                     return False
@@ -81,27 +89,78 @@ class Board:
                 block.append(self.data[y][x])
         return block
 
-    def solve_one(self, get_group: Callable) -> None:
-        '''Solves for all of a group (rows, columns, or blocks) that has one cell empty.'''
-        assert self.check_board(), "Error in board!"
-        for i in range(9):
-            group = get_group(i) # Get group
-            empty_cells = [cell for cell in group if cell.value == '.']
-            if len(empty_cells) == 1:  # Only one empty cell
-                missing_value = VALID_NUMS.difference({cell.value for cell in group}).pop()
-                empty_cells[0].set(missing_value)
 
 def init_board(board_obj: Board, data:list) -> None:
     '''Fills board with either numbers or .'''
     # Input checks
-    assert len(data) == 9, f"Input data has length {len(data)}."
+    assert len(data) == board_obj.n, f"Input data has length {len(data)}."
     assert all(isinstance(item, str) for item in data), "Input data must contain only strings."
-    assert all(len(item) == 9 for item in data), "Input data must have 9 items in each string."
-    for i in range(9):
+    assert all(len(item) == board_obj.n for item in data), "Input data must have 9 items in each string."
+    print('Setting board...')
+    for i in range(board_obj.n):
         rowdata = list(data[i])
         board_obj.set_row(rowdata, i)
 
-board = Board()
+
+def solve_one(_board: Board, get_group: Callable) -> bool:
+    '''Solves for all of a board's naked singles for a group (rows, columns, or blocks).
+    Returns if change to board was made.'''
+    assert _board.check_board(), f"Error in board!\n{_board}"
+    changed = False
+    valid_nums = {str(i) for i in range(1, _board.n + 1)}
+    for i in range(_board.n):
+        group = get_group(i) # Get group
+        empty_cells = [cell for cell in group if cell.value == '.']
+        if len(empty_cells) == 1: # Only one empty cell
+            missing_value = valid_nums.difference({cell.value for cell in group}).pop()
+            empty_cells[0].set(missing_value)
+            changed = True
+    return changed
+
+
+def last_digit(_board: Board) -> bool:
+    '''Solves for the last digit for rows, columns, and blocks until no more progress can be made.
+    Returns True if any changes were made to the board.'''
+    total_changed = False
+    while True:
+        row_changed = solve_one(_board, _board.get_row)
+        col_changed = solve_one(_board, _board.get_col)
+        block_changed = solve_one(_board, _board.get_block)
+        iteration_changed = row_changed or col_changed or block_changed
+        total_changed = total_changed or iteration_changed
+        if not iteration_changed:
+            break
+    return total_changed
+
+
+def find_pencil(_board: Board) -> None:
+    '''Finds pencil marks for each cell.'''
+    
+
+def is_solved(_board: Board) -> bool:
+    '''Checks if board is solved (no empty cells and valid).'''
+    assert _board.check_board(), f"Error in board!\n{_board}"
+    return not any(cell.value == '.' for row in _board.data for cell in row)
+
+
+def solver(_board: Board) -> Board:
+    '''Main sudoku solver logic.'''
+    print('Solving board...')
+    while True:
+        # Check if solved
+        if is_solved(_board):
+            print("Board solved!")
+            return _board
+        changed = []
+        # Try all solving techniques
+        changed.append(last_digit(_board))
+        # If nothing changed then unsolvable with current logic
+        if not any(changed):
+            print("Board cannot be solved with current logic!")
+            return _board
+            
+
+board = Board(9)
 unsolved_data = ['...26.7.1',
         '68..7..9.',
         '19...45..',
@@ -111,7 +170,6 @@ unsolved_data = ['...26.7.1',
         '..93...74',
         '.4..5..36',
         '7.3.18...']
-
 solved_data = ['435269781',
         '682571493',
         '197834562',
@@ -121,7 +179,6 @@ solved_data = ['435269781',
         '519326874',
         '248957136',
         '763418259']
-
 test_data = ['4352697.1',
         '68.571493',
         '197834562',
@@ -132,9 +189,10 @@ test_data = ['4352697.1',
         '24895.136',
         '763418259']
 
-init_board(board, test_data)
-print('---')
+init_board(board, unsolved_data)
+print('\n---\n')
 print(board)
-board.solve_one(board.get_block)
-print('---')
+print('\n')
+board = solver(board)
+print('\n---\n')
 print(board)
