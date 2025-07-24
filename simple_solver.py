74bb10438b064345d6b3c0bef761f66cf0cf3e9b
Amelia Dayton <daytonamelia@gmail.com>
2025-07-24 12:43:59 -0700

    Hidden singles implemented

--- simple_solver.py
+++ simple_solver.py
@@ -3,6 +3,7 @@ Simple soduku solver.
 https://hodoku.sourceforge.net/en/techniques.php
 '''
 # --- Imports ---
+import time
 from typing import Callable
 
 # --- Classes ---
@@ -32,11 +33,11 @@ class Cell:
         '''Sets the value of a cell to a new value.'''
         print(f"INFO: Setting cell at ({self.x}, {self.y}) to {_value}")
         self.value = _value
-        
+ 
     def set_pencil(self, _marks:set) -> None:
         '''Update the cell's pencil marks.'''
-        self.pencil.update(_marks)
-        
+        self.pencil = _marks
+  
 
 class Board:
     '''
@@ -118,23 +119,9 @@ def is_solved(_board: Board) -> bool:
     return not any(cell.value == '.' for row in _board.data for cell in row)
 
 
-def solve_one(_board: Board, get_group: Callable) -> bool:
-    '''Solves for all of a board's last digits for a group (rows, columns, or blocks).
-    Returns if change to board was made.'''
-    assert _board.check_board(), f"Error in board!\n{_board}"
-    changed = False
-    for i in range(_board.n):
-        group = get_group(i) # Get group
-        empty_cells = [cell for cell in group if cell.value == '.']
-        if len(empty_cells) == 1: # Only one empty cell
-            missing_value = VALID_NUMS.difference({cell.value for cell in group}).pop()
-            empty_cells[0].set(missing_value)
-            changed = True
-    return changed
-    
-
+# --- Pencil Functions ---
 def find_pencil(_board: Board, _cell: Cell) -> int:
-    '''Find pencil marks for a cell.'''
+    '''Find pencil marks for a cell and returns how many marks calculated for that cell.'''
     if _cell.value != '.':
         return 0
     row = set([cell.value for cell in _board.get_row(_cell.y) if cell.value != '.'])
@@ -146,10 +133,33 @@ def find_pencil(_board: Board, _cell: Cell) -> int:
     return len(marks)
 
 
+def update_pencil(_board:Board) -> None:
+    '''Update pencil marks for the entire board.'''
+    for y in range(_board.n):  # y = row index
+        for x in range(_board.n):  # x = column index
+            cell = _board.get_cell(x, y)
+            if cell.value != '.':
+                continue
+            find_pencil(_board, cell)
+
+
 # --- Solver Functions ---
 def last_digit(_board: Board) -> bool:
     '''Solves for the last digit for rows, columns, and blocks until no more progress can be made.
     Returns True if any changes were made to the board.'''
+    def solve_one(_board: Board, get_group: Callable) -> bool:
+        '''Solves for all of a board's last digits for a group (rows, columns, or blocks).
+        Returns if change to board was made.'''
+        changed = False
+        for i in range(_board.n):
+            group = get_group(i) # Get group
+            empty_cells = [cell for cell in group if cell.value == '.']
+            if len(empty_cells) == 1: # Only one empty cell
+                missing_value = VALID_NUMS.difference({cell.value for cell in group}).pop()
+                empty_cells[0].set(missing_value)
+                changed = True
+        return changed
+
     total_changed = False
     while True:
         row_changed = solve_one(_board, _board.get_row)
@@ -162,8 +172,8 @@ def last_digit(_board: Board) -> bool:
     return total_changed
 
 
-def hidden_naked_single(_board:Board) -> bool:
-    '''Solves for hidden and naked singles for rows, columns, and blocks until no more progress can be made.
+def naked_single(_board:Board) -> bool:
+    '''Solves for naked singles for rows, columns, and blocks until no more progress can be made.
     Returns True if any changes were made to the board.'''
     total_changed = False
     while True:
@@ -182,6 +192,64 @@ def hidden_naked_single(_board:Board) -> bool:
             break
     return total_changed
 
+
+def hidden_single(_board:Board) -> bool:
+    '''Solves for hidden singles for rows, columns, and blocks until no more progress can be made.
+    Return True if any changes were made to the board.'''
+    def solve_hidden(_board:Board, get_group:Callable) -> bool:
+        '''Checks and solves for all of a board's hidden singles for a group (rows, columns, or blocks).
+        Returns if change to board was made.'''
+        changed = False
+        for i in range(_board.n):
+            group = get_group(i)
+            # Get marks for empty cells in group
+            cell_marks = []
+            for cell in group:
+                if cell.value != '.':
+                    cell_marks.append({''})
+                    continue
+                else:
+                    cell_marks.append(cell.pencil)
+            # Find cells with hidden singles
+            mark_counts = {} # Count occurences
+            for marks in cell_marks:
+                for value in marks:
+                    mark_counts[value] = mark_counts.get(value, 0) + 1
+            hidden_singles = [] # Find hidden singles
+            for i, marks in enumerate(cell_marks):
+                unique_marks = {value for value in marks if mark_counts[value] == 1}
+                if unique_marks:
+                    hidden_singles.append((i, unique_marks))
+            # Solve for hidden singles
+            if len(hidden_singles) > 0:
+                for single in hidden_singles:
+                    index = single[0]
+                    value = single[1].pop()
+                    if value not in VALID_NUMS:
+                        continue
+                    group[index].set(value)
+                    changed = True
+        return changed
+
+    total_changed = False
+    while True:
+        update_pencil(_board)
+        row_changed = solve_hidden(_board, _board.get_row)
+        if row_changed:
+            update_pencil(_board)
+        col_changed = solve_hidden(_board, _board.get_col)
+        if col_changed:
+            update_pencil(_board)
+        block_changed = solve_hidden(_board, _board.get_block)
+        if block_changed:
+            update_pencil(_board)
+        iteration_changed = row_changed or col_changed or block_changed
+        total_changed = total_changed or iteration_changed
+        if not iteration_changed:
+            break
+    return total_changed
+
+
 # --- Main Functions ---
 def solver(_board: Board) -> Board:
     '''Main sudoku solver logic.'''
@@ -189,6 +257,7 @@ def solver(_board: Board) -> Board:
     while True:
         print('-')
         print(iter)
+        # Iteration for debug
         iter += 1
         # Check if solved
         if is_solved(_board):
@@ -199,7 +268,11 @@ def solver(_board: Board) -> Board:
         print("Checking for last digits...")
         changed.append(last_digit(_board))
         print("Checking for naked singles...")
-        changed.append(hidden_naked_single(_board))
+        changed.append(naked_single(_board))
+        print("Checking for hidden singles...")
+        changed.append(hidden_single(_board))
+        # Check board
+        assert _board.check_board(), f"Error in board!\n{_board}"
         # If nothing changed then unsolvable with current logic
         if not any(changed):
             print("Board cannot be solved with current logic!")
@@ -240,17 +313,7 @@ if __name__ == '__main__':
         '519326874',
         '248957136',
         '763418259']
-    hiddensingle_data = [
-        '.28..7...',
-        '.16.83.7.',
-        '....2.851',
-        '13729....',
-        '...73....',
-        '....463.7',
-        '29..7....',
-        '...86.14.',
-        '...3..7..']
     
     N = 9
     VALID_NUMS = {str(i) for i in range(1, N + 1)}
-    main(N, hiddensingle_data)
+    main(N, unsolved_data)
